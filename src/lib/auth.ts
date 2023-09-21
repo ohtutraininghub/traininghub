14962bcb4c291d5ec0ad06abf19a1931c69da979
Mikael Moilanen <mikael.moilanen@gmail.com>
2023-09-21 22:12:10 +0300

    Refactor to use nullish coalescing

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,16 @@
-import NextAuth from 'next-auth'
+import NextAuth from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ||'',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ||''
-    })
-  ]
-}
+      clientId: process.env?.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env?.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+  ],
+};
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
