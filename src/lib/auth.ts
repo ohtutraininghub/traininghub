c725b0dcdab71f3d42d6b6d4538df3aa863342d6
Pia Manns <pia.manns@gmail.com>
2023-09-22 18:01:23 +0300

    Combine jwt-session strategy with database integration
    Co-authored by: AK <anketola@users.noreply.github.com>

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,3 @@
-
 import type { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { prisma } from './prisma';
@@ -7,10 +6,13 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-
       clientId: process.env?.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env?.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+    maxAge: 7 * 24 * 60 * 60,
+  },
 };
-
