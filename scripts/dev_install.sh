16538626de96f882d069dd3948dc17c266669666
DeeCaaD <perttu.kangas@hotmail.fi>
2023-09-28 16:26:56 +0300

    Wait for db to start before next step

--- scripts/dev_install.sh
+++ scripts/dev_install.sh
@@ -4,6 +4,10 @@ npm install
 # Start and build database
 docker compose up -d --build
 
+# Loop every 1s until Postgres has fully started
+DATABASE_CONTAINER="postgres-dev"
+until docker exec $DATABASE_CONTAINER pg_isready ; do sleep 1 ; done
+
 # Run database migrations
 npx prisma migrate dev
 
--- scripts/dev_start.sh
+++ scripts/dev_start.sh
@@ -2,6 +2,10 @@
 # Started as background task
 npm run dev:docker
 
+# Loop every 1s until Postgres has fully started
+DATABASE_CONTAINER="postgres-dev"
+until docker exec $DATABASE_CONTAINER pg_isready ; do sleep 1 ; done
+
 # When CTRL + C close database
 trap "docker compose down" INT TERM
 
