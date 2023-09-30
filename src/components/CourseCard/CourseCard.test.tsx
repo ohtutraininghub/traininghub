7e2b21fd2f77a9d5d6905095fb9895c4e8b2c1ea
tommivk <tommikokk@gmail.com>
2023-09-30 18:52:07 +0300

    add tests for CourseCard

---
+++ src/components/CourseCard/CourseCard.test.tsx
@@ -0,0 +1,35 @@
+import { renderWithTheme } from '@/lib/test-utils';
+import CourseCard from './CourseCard';
+import { Course } from '@prisma/client';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+const course: Course = {
+  id: '22',
+  name: 'Test course',
+  description: 'A Test course',
+  startDate: new Date(),
+  endDate: new Date(),
+  maxStudents: 42,
+};
+
+beforeEach(() => {
+  renderWithTheme(<CourseCard course={course} />);
+});
+
+describe('CourseCard tests', () => {
+  it('Course data should be displayed correctly', async () => {
+    expect(screen.getByText(course.name)).toBeVisible();
+    expect(screen.getByText(course.startDate.toLocaleString())).toBeVisible();
+    expect(
+      screen.getByText(`0 / ${course.maxStudents}`, { exact: false })
+    ).toBeVisible();
+    expect(screen.getByText(course.description)).not.toBeVisible();
+  });
+
+  it('Course description should be visible when collapse is opened', async () => {
+    const toggle = screen.getByRole('button');
+    await userEvent.click(toggle);
+    expect(screen.getByText(course.description)).toBeVisible();
+  });
+});
