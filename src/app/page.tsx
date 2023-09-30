f38d6c27d0d6e3af2c81c7795743cbc7f8ff5c96
tommivk <tommikokk@gmail.com>
2023-09-30 23:37:44 +0300

    use div instead of Box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,22 @@
-import { Box, Typography } from '@mui/material';
 import DeviceHubIcon from '@mui/icons-material/DeviceHub';
 import CourseForm from '@/components/CourseForm/CourseForm';
 import CourseList from '@/components/CourseList/CourseList';
 
 export default async function HomePage() {
   return (
-    <Box
-      sx={{
+    <div
+      style={{
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        pb: 20,
-        px: 2,
+        padding: '0px 16px 100px 16px',
       }}
     >
       <DeviceHubIcon fontSize="small" />
       <h2>Add new Course</h2>
       <CourseForm />
       <CourseList />
-    </Box>
+    </div>
   );
 }
