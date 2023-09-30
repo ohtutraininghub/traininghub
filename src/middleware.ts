67679a7af7de6cf177ffc2e6d6072bff4858a272
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-09-30 22:12:14 +0300

    combine nextauth and i18n middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,28 @@
-export { default } from 'next-auth/middleware';
-
-/*
-
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 import { checkForMissingLocale, getLocale } from '@i18n/i18n';
+import { withAuth } from 'next-auth/middleware';
 
-export function middleware(request: NextRequest) {
+export default withAuth(function middleware(request) {
   const { pathname } = request.nextUrl;
+  if (
+    !pathname.startsWith('/_next') || // exclude Next.js internals
+    !pathname.startsWith('/api') || // exclude all API routes
+    !pathname.startsWith('/static') // exclude static files
+  ) {
+    const pathnameIsMissingLocale = checkForMissingLocale(pathname);
 
-  const pathnameIsMissingLocale = checkForMissingLocale(pathname);
+    // Redirect if there is no locale
+    if (pathnameIsMissingLocale) {
+      const locale = getLocale(request);
 
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-
-    // e.g. incoming request is /products
-    // The new URL is now /en/products
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
-        request.url
-      )
-    );
+      // e.g. incoming request is /products
+      // The new URL is now /en/products
+      return NextResponse.redirect(
+        new URL(
+          `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
+          request.url
+        )
+      );
+    }
   }
-}
-
-export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};
-
-
-*/
+});
