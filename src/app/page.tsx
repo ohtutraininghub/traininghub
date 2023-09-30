4c92cb52a3fd415d9c923e3c3bba5e346f4b7755
vaisajuh <juha.vaisanen@gmail.com>
2023-09-30 12:30:00 +0300

    Fix frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,18 @@ export default async function HomePage() {
   return (
     <Box
       sx={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        width: '100%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         pb: 20,
       }}
     >
-      <Typography variant="h1" color="primary">
-        Hello TrainingHub
-      </Typography>
-      <Box sx={{ display: 'flex', gap: 1 }}>
-        <Typography color="primary.main">Primary color</Typography>
-        <Typography color="secondary.main">Secondary color</Typography>
-        <Typography color="info.main">Info</Typography>
-        <Typography color="darkBlue.main">Dark blue</Typography>
-        <Typography color="black.main">Black</Typography>
-        <Typography sx={{ backgroundColor: 'info.main' }} color="white.main">
-          White
-        </Typography>
-      </Box>
       <DeviceHubIcon fontSize="small" />
-      <h1>Add new Course</h1>
+      <h2>Add new Course</h2>
       <CourseForm />
       <CourseList />
     </Box>
