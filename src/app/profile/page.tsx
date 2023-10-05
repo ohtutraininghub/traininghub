b9360a4a1e496926126f43aa57c088fb68ab08ca
anketola <antti@anttiketola.com>
2023-10-05 09:04:14 +0300

    Fetch user data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,30 @@
-export default function ProfilePage() {
-  return <div>yeet</div>;
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/lib/auth';
+import { prisma } from '@/lib/prisma';
+import Box from '@mui/material/Box/Box';
+
+export default async function ProfilePage() {
+  const session = await getServerSession(authOptions);
+  const usermail = session?.user?.email;
+  let userData = null;
+  if (usermail) {
+    userData = await prisma.user.findUnique({
+      where: {
+        email: usermail,
+      },
+      //      include: {
+      //        courses: true
+      //      },
+    });
+  }
+
+  console.log(userData);
+
+  return (
+    <Box>
+      {userData?.name} <br />
+      {userData?.email} <br />
+      {userData?.image} <br />
+    </Box>
+  );
 }
