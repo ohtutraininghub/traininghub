38ee41cc5e98ec56c789360b032820ee79e4dddc
Pia Manns <pia.manns@gmail.com>
2023-10-07 14:32:44 +0300

    Add layout for admin pages

--- src/app/admin/create-tag/page.tsx
+++ src/app/admin/create-tag/page.tsx
@@ -1,7 +1,6 @@
 import TagForm from '@/components/TagForm/TagForm';
 import TagList from '@/components/TagList/TagList';
 import { prisma } from '@/lib/prisma';
-import Box from '@mui/material/Box';
 
 export default async function CreateTag() {
   const tags = await prisma.tag.findMany({
@@ -9,20 +8,11 @@ export default async function CreateTag() {
   });
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        mx: 1,
-        my: 1,
-      }}
-    >
+    <>
       <h1>Add a new tag</h1>
       <TagForm />
       <h3>Existing tags:</h3>
       <TagList tags={tags} />
-    </Box>
+    </>
   );
 }
