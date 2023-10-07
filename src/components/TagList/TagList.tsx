9ea225c3a9403c064d206d709bd2165d11fa57ee
Pia Manns <pia.manns@gmail.com>
2023-10-07 16:53:21 +0300

    Add styling to list of existing tags

--- src/components/TagList/TagList.tsx
+++ src/components/TagList/TagList.tsx
@@ -1,4 +1,5 @@
 import { Prisma } from '@prisma/client';
+import Box from '@mui/material/Box';
 
 type TagPrismaType = Prisma.TagGetPayload<Prisma.TagDefaultArgs>;
 
@@ -8,9 +9,32 @@ type TagListProps = {
 
 export default function TagList({ tags }: TagListProps) {
   return (
-    <ul>
+    <ul
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        padding: 0,
+      }}
+    >
       {tags.map((tag) => (
-        <li key={tag.name}>{tag.name}</li>
+        <li
+          key={tag.name}
+          style={{
+            display: 'inline',
+            margin: '0 0.5em 1.2em 0',
+          }}
+        >
+          <Box
+            component="span"
+            sx={{
+              border: '1px solid',
+              borderRadius: '4px',
+              p: '0.5em',
+            }}
+          >
+            {tag.name}
+          </Box>
+        </li>
       ))}
     </ul>
   );
