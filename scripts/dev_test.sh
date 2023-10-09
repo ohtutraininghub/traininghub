f82cca4715724c8c77986ba7812859f9076b9d57
DeeCaaD <perttu.kangas@hotmail.fi>
2023-10-09 13:55:31 +0300

    Add test docker db

---
+++ scripts/dev_test.sh
@@ -0,0 +1,23 @@
+# Take additional args passed
+ARGS=$1
+
+echo $ARGS
+
+# Start and build if there is changes
+# Started as background task
+npm run test:docker
+
+# Loop every 1s until Postgres has fully started
+DATABASE_CONTAINER="postgres-test"
+until docker exec $DATABASE_CONTAINER pg_isready ; do sleep 1 ; done
+
+# When CTRL + C close database
+trap "docker compose -f docker-compose.test.yml down" INT TERM
+
+# Run database migrations
+npx prisma migrate dev --skip-seed
+
+npm run test:watch $ARGS
+
+# When quit from watch "normally"
+docker compose -f docker-compose.test.yml down
