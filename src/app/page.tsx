6b1b0a411a81c0870294444a162931388d7aaea2
Pia Manns <pia.manns@gmail.com>
2023-10-12 22:45:50 +0300

    Add temporary button to first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import CourseList from '@/components/CourseList/CourseList';
 import { notFound } from 'next/navigation';
 import { getCourses, getEnrolledCourseIdsByUserId } from '@/lib/prisma/courses';
 import { getServerAuthSession } from '@/lib/auth';
+import Link from 'next/link';
+import Button from '@mui/material/Button';
 
 export const dynamic = 'force-dynamic';
 
@@ -33,6 +35,12 @@ export default async function HomePage({ searchParams }: Props) {
         padding: '0px 16px 100px 16px',
       }}
     >
+      <Link href="/admin/create-tag">
+        <Button color="secondary" variant="contained">
+          Add New Tag
+        </Button>
+      </Link>
+
       <h2>Add new Course</h2>
       <CourseForm />
       <CourseList
