274948c3ea46c83bd8d7fd86eac569ff140eff05
Pia Manns <pia.manns@gmail.com>
2023-10-12 22:18:49 +0300

    Add unit tests for tag route

--- src/lib/zod/tags.ts
+++ src/lib/zod/tags.ts
@@ -4,7 +4,7 @@ export const tagSchema = z.object({
   name: z
     .string()
     .min(1, 'A tag name is required')
-    .max(50, 'The maximum length for tag is 50 characters'),
+    .max(50, 'The maximum length for a tag is 50 characters'),
 });
 
 export type TagSchemaType = z.infer<typeof tagSchema>;
