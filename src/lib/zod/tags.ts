f7cea499fdee794b17f1e994c4ed04d75d22ae91
Pia Manns <pia.manns@gmail.com>
2023-10-14 21:48:52 +0300

    Add check for extra spaces to tag zod schema
    Co-authored-by: AK <39833064+anketola@users.noreply.github.com>

--- src/lib/zod/tags.ts
+++ src/lib/zod/tags.ts
@@ -1,10 +1,20 @@
 import z from 'zod';
 
 export const tagSchema = z.object({
-  name: z
+ name: z
     .string()
-    .min(1, 'A tag name is required')
-    .max(50, 'The maximum length for a tag is 50 characters'),
+    .refine(
+      (value) => value.trim() !== '',
+      { message: 'A tag name is required' }
+    )
+    .refine(
+      (value) => value.trim().length <= 50,
+      { message: 'The maximum length for a tag is 50 characters' }
+    )
+    .refine(
+      (value) => !/\s{2,}/.test(value),
+      { message: 'Consecutive spaces are not allowed' }
+    ),
 });
 
 export type TagSchemaType = z.infer<typeof tagSchema>;
