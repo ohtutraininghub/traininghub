54c23d885bbe8e0c904c0fa2b5bf2f8011319e82
DeeCaaD <perttu.kangas@hotmail.fi>
2023-10-14 16:32:23 +0300

    Test da new format

--- src/lib/zod/courses.ts
+++ src/lib/zod/courses.ts
@@ -1,35 +1,32 @@
 import z from 'zod';
 
-const courseSchemaObject = z.object({
-  name: z.string().min(1, 'Name is required'),
-  description: z.string().min(1, 'Description is required'),
-  startDate: z.string().min(1, 'Start date is required').pipe(z.coerce.date()),
-  endDate: z.string().min(1, 'End date is required').pipe(z.coerce.date()),
-  maxStudents: z.number().min(1, 'Max students is required'),
-});
+export const courseSchema = z
+  .object({
+    name: z.string().min(1, 'Name is required'),
+    description: z.string().min(1, 'Description is required'),
+    startDate: z
+      .string()
+      .min(1, 'Start date is required')
+      .pipe(z.coerce.date()),
+    endDate: z.string().min(1, 'End date is required').pipe(z.coerce.date()),
+    maxStudents: z.number().min(1, 'Max students is required'),
+  })
+  .strict()
+  .refine(
+    ({ startDate, endDate }) =>
+      new Date(startDate).getTime() <= new Date(endDate).getTime(),
+    {
+      message: 'The end date cannot be before the start date',
+      path: ['endDate'],
+    }
+  );
 
-const courseSchemaObjectWithId = courseSchemaObject.extend({
-  id: z.string().min(1, 'Id is required'),
-});
+export const courseSchemaWithId = courseSchema.and(
+  z.object({ id: z.string().min(1, 'Id is required') })
+);
 
-const withRefine = (schema: typeof courseSchemaObject) => {
-  return schema
-    .strict()
-    .refine(
-      ({ startDate, endDate }) =>
-        new Date(startDate).getTime() <= new Date(endDate).getTime(),
-      {
-        message: 'The end date cannot be before the start date',
-        path: ['endDate'],
-      }
-    );
-};
-
-export const courseSchema = withRefine(courseSchemaObject);
-export const courseSchemaWithId = withRefine(courseSchemaObjectWithId);
-
-export type CourseSchemaType = z.infer<typeof courseSchemaObject>;
-export type CourseSchemaWithIdType = z.infer<typeof courseSchemaObjectWithId>;
+export type CourseSchemaType = z.infer<typeof courseSchema>;
+export type CourseSchemaWithIdType = z.infer<typeof courseSchemaWithId>;
 
 export const courseSignupSchema = z.string(
   z.string().min(1, 'Course id is required')
