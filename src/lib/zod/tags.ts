97a125748bf999576d3da18f454e8ff6faa20696
Pia Manns <pia.manns@gmail.com>
2023-10-14 22:01:34 +0300

    Fix code style issues

--- src/lib/zod/tags.ts
+++ src/lib/zod/tags.ts
@@ -1,20 +1,17 @@
 import z from 'zod';
 
 export const tagSchema = z.object({
- name: z
+  name: z
     .string()
-    .refine(
-      (value) => value.trim() !== '',
-      { message: 'A tag name is required' }
-    )
-    .refine(
-      (value) => value.trim().length <= 50,
-      { message: 'The maximum length for a tag is 50 characters' }
-    )
-    .refine(
-      (value) => !/\s{2,}/.test(value),
-      { message: 'Consecutive spaces are not allowed' }
-    ),
+    .refine((value) => value.trim() !== '', {
+      message: 'A tag name is required',
+    })
+    .refine((value) => value.trim().length <= 50, {
+      message: 'The maximum length for a tag is 50 characters',
+    })
+    .refine((value) => !/\s{2,}/.test(value), {
+      message: 'Consecutive spaces are not allowed',
+    }),
 });
 
 export type TagSchemaType = z.infer<typeof tagSchema>;
