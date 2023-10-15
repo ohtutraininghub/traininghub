3b168b8bfc13ce8f3cacccc08bb786b445933263
Pia Manns <pia.manns@gmail.com>
2023-10-15 13:07:33 +0300

    Add unit tests for tag form and list components

--- src/components/TagList/index.tsx
+++ src/components/TagList/index.tsx
@@ -1,5 +1,6 @@
 import { Prisma } from '@prisma/client';
 import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
 
 type TagPrismaType = Prisma.TagGetPayload<Prisma.TagDefaultArgs>;
 
@@ -8,34 +9,42 @@ type TagListProps = {
 };
 
 export default function TagList({ tags }: TagListProps) {
-  return (
-    <ul
-      style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        padding: 0,
-      }}
-    >
-      {tags.map((tag) => (
-        <li
-          key={tag.name}
-          style={{
-            display: 'inline',
-            margin: '0 0.5em 1.2em 0',
-          }}
-        >
-          <Box
-            component="span"
-            sx={{
-              border: '1px solid',
-              borderRadius: '4px',
-              p: '0.5em',
+  if (tags.length == 0) {
+    return (
+      <Typography variant="body1" mt={1}>
+        (No tags added)
+      </Typography>
+    );
+  } else {
+    return (
+      <ul
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          padding: 0,
+        }}
+      >
+        {tags.map((tag) => (
+          <li
+            key={tag.name}
+            style={{
+              display: 'inline',
+              margin: '0 0.5em 1.2em 0',
             }}
           >
-            {tag.name}
-          </Box>
-        </li>
-      ))}
-    </ul>
-  );
+            <Box
+              component="span"
+              sx={{
+                border: '1px solid',
+                borderRadius: '4px',
+                p: '0.5em',
+              }}
+            >
+              {tag.name}
+            </Box>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
