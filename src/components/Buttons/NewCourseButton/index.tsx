c48f3acd17df066db7d1c36c7cd1b75c2745d229
Mikael Moilanen <mikael.moilanen@gmail.com>
2023-10-18 21:31:09 +0300

    Add missing namespace to coursebutton

--- src/components/Buttons/NewCourseButton/index.tsx
+++ src/components/Buttons/NewCourseButton/index.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link';
 interface Props extends DictProps {}
 
 export default function NewCourseButton({ lang }: Props) {
-  const { t } = useTranslation(lang);
+  const { t } = useTranslation(lang, 'components');
   return (
     <>
       <Typography variant="h4">{t('NewCourseButton.actions')}</Typography>
