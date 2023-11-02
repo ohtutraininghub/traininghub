6839342a1fd71ea23d94505b383f3cb40213ec75
DeeCaaD <perttu.kangas@hotmail.fi>
2023-11-02 13:02:23 +0200

    Minor adjustments

--- src/app/[lang]/course/create/page.tsx
+++ src/app/[lang]/course/create/page.tsx
@@ -1,14 +1,14 @@
 import CourseForm from '@/components/CourseForm/CourseForm';
-import { Locale } from '@/lib/i18n/i18n-config';
+import { Locale } from '@i18n/i18n-config';
 import { getTags } from '@/lib/prisma/tags';
+
+export const dynamic = 'force-dynamic';
 
 interface Props {
   params: {
     lang: Locale;
   };
 }
-
-export const dynamic = 'force-dynamic';
 
 export default async function NewCoursePage({ params }: Props) {
   const tags = await getTags();
