4fe2d14715310b9cac401b1aa96726a7b73164c2
anketola <antti@anttiketola.com>
2023-11-04 12:52:09 +0200

    Enhance placeholder list styling

--- src/components/CourseList/TemporaryListView.tsx
+++ src/components/CourseList/TemporaryListView.tsx
@@ -2,6 +2,9 @@ import { Box, List, ListItem, ListItemText } from '@mui/material';
 import { CourseWithTagsAndStudentCount } from '@/lib/prisma/courses';
 import CourseModal from '../CourseModal/CourseModal';
 import { DictProps, useTranslation } from '@/lib/i18n';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import PeopleIcon from '@mui/icons-material/People';
+import Divider from '@mui/material/Divider';
 
 interface CourseListProps extends DictProps {
   courses: CourseWithTagsAndStudentCount[];
@@ -31,16 +34,60 @@ export default async function TemporaryListView({
         editCourseLabel={t('EditButton.editCourse')}
       />
 
-      <Box>
+      <Box
+        style={{
+          background: 'rgba(0, 0, 0, 0.1)',
+          padding: '0px',
+        }}
+      >
         <List>
-          {courses.map((course) => (
-            <ListItem key={course.id}>
-              <ListItemText
-                primary={course.name}
-                primaryTypographyProps={{ style: { color: 'white' } }}
-                secondary={`Start Date: ${course.startDate}, Students: ${course._count.students}`}
-              />
-            </ListItem>
+          {courses.map((course, index) => (
+            <div key={course.id}>
+              <ListItem
+                style={{
+                  background: 'rgba(0, 0, 0, 0.2)',
+                  marginBottom: '10px',
+                }}
+              >
+                <ListItemText
+                  primary={course.name}
+                  primaryTypographyProps={{
+                    style: {
+                      color: 'white',
+                      fontSize: '18px',
+                      fontWeight: 400,
+                    },
+                  }}
+                  secondary={
+                    <span style={{ marginBottom: '8px', display: 'block' }}>
+                      <CalendarTodayIcon
+                        sx={{
+                          fontSize: '0.8rem',
+                          marginRight: '8px',
+                          display: 'inline',
+                        }}
+                      />
+                      Start Date: {course.startDate.toISOString()}
+                      <br />
+                      <PeopleIcon
+                        sx={{
+                          fontSize: '0.8rem',
+                          marginRight: '8px',
+                          display: 'inline',
+                        }}
+                      />
+                      Enrolls {course._count.students}
+                    </span>
+                  }
+                  secondaryTypographyProps={{
+                    style: {
+                      color: 'white',
+                    },
+                  }}
+                />
+              </ListItem>
+              {index < courses.length - 1 && <Divider />}
+            </div>
           ))}
         </List>
       </Box>
