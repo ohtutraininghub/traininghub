2cc28a91fa0e256e1987d2763b6dd684ecf406d3
Pia Manns <pia.manns@gmail.com>
2023-11-18 21:53:26 +0200

    Add check to api allowing only admins to change user roles

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,18 +1,37 @@
 import { prisma } from '@/lib/prisma';
-import { StatusCodeType, successResponse } from '@/lib/response/responseUtil';
+import {
+  StatusCodeType,
+  errorResponse,
+  successResponse,
+} from '@/lib/response/responseUtil';
 import { NextRequest } from 'next/server';
+import { getServerAuthSession } from '@/lib/auth';
+import { isAdmin } from '@/lib/auth-utils';
+import { handleCommonErrors } from '@/lib/response/errorUtil';
 
 export async function PUT(request: NextRequest) {
   const data = await request.json();
   const { userId, newRole } = data;
 
-  await prisma.user.update({
-    where: { id: userId },
-    data: { role: newRole },
-  });
+  try {
+    const { user } = await getServerAuthSession();
+    if (!isAdmin(user)) {
+      return errorResponse({
+        message: 'Forbidden',
+        statusCode: StatusCodeType.FORBIDDEN,
+      });
+    }
 
-  return successResponse({
-    message: 'User access role succesfully changed!',
-    statusCode: StatusCodeType.OK,
-  });
+    await prisma.user.update({
+      where: { id: userId },
+      data: { role: newRole },
+    });
+
+    return successResponse({
+      message: 'User access role succesfully changed!',
+      statusCode: StatusCodeType.OK,
+    });
+  } catch (error: unknown) {
+    return handleCommonErrors(error);
+  }
 }
