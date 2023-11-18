aceb6d3f185b2b29250c70ceaf6b6a9c565e9c42
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-11-18 17:27:56 +0200

    remove explicit strictmode declaration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
   webpack: (config) => {
     config.externals = [...config.externals, 'canvas', 'jsdom', 'node-fetch'];
     return config;
