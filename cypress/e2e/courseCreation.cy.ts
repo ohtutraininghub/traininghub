e72dc6e0db99d01ab7f55e9b972625b5ed27a713
tommivk <tommikokk@gmail.com>
2023-11-22 21:03:44 +0200

    add tests

--- cypress/e2e/courseCreation.cy.ts
+++ cypress/e2e/courseCreation.cy.ts
@@ -98,6 +98,28 @@ describe('Course creation', () => {
     cy.contains('Start date cannot be in the past');
   });
 
+  it('should not be possible for last enroll date to be after the end date', () => {
+    cy.login('trainer@test.com', 'TRAINER');
+    cy.visit('/course/create');
+    cy.getCy('courseFormEndDate').type('2050-06-01T00:00');
+    cy.getCy('courseFormLastEnrollDate').type('2050-06-02T00:00');
+    cy.getCy('courseFormSubmit').click();
+    cy.contains(
+      'The last date to enroll cannot be after the end date of the course'
+    );
+  });
+
+  it('should not be possible for last cancel date to be after the end date', () => {
+    cy.login('trainer@test.com', 'TRAINER');
+    cy.visit('/course/create');
+    cy.getCy('courseFormEndDate').type('2050-06-01T00:00');
+    cy.getCy('courseFormLastCancelDate').type('2050-06-02T00:00');
+    cy.getCy('courseFormSubmit').click();
+    cy.contains(
+      'The last date to cancel enrollment cannot be after the end date of the course'
+    );
+  });
+
   it('should not be possible to access course creation page as a trainee', () => {
     cy.login('trainee@test.com', 'TRAINEE');
     cy.visit('/course/create');
