f1f9c245a4c7134b927d63dbedcc94889d606f61
vaisajuh <juha.vaisanen@gmail.com>
2023-11-25 21:00:37 +0200

    Replace date inputs in the form with DateTimePickers

--- src/components/CourseForm/CourseForm.tsx
+++ src/components/CourseForm/CourseForm.tsx
@@ -31,6 +31,9 @@ import { useTranslation } from '@i18n/client';
 import { DictProps } from '@i18n/index';
 import RichTextEditor from '@/components/TextEditor';
 import { useEffect, useState } from 'react';
+import { DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from 'dayjs';
 
 interface CourseFormProps extends DictProps {
   tags: Tag[];
@@ -56,6 +59,7 @@ export default function CourseForm({
     formState: { errors, isSubmitting },
     handleSubmit,
     reset,
+    setValue,
   } = useForm<FormType>({
     resolver: zodResolver(isEditMode ? courseSchemaWithId : courseSchema),
     defaultValues: {
@@ -72,8 +76,10 @@ export default function CourseForm({
     },
   });
 
-  const [lastEnrollDate, setLastEnrollDate] = useState<Date | ''>('');
-  const [lastCancelDate, setLastCancelDate] = useState<Date | ''>('');
+  const [startDate, setStartDate] = useState<Date | Dayjs | ''>('');
+  const [endDate, setEndDate] = useState<Date | Dayjs | ''>('');
+  const [lastEnrollDate, setLastEnrollDate] = useState<Date | Dayjs | ''>('');
+  const [lastCancelDate, setLastCancelDate] = useState<Date | Dayjs | ''>('');
 
   useEffect(() => {}, [lastEnrollDate, lastCancelDate]);
 
@@ -96,11 +102,12 @@ export default function CourseForm({
   };
 
   const updateValue = (
-    newValue: Date | '',
+    newValue: Date | Dayjs | '',
     currentValue: Date | null | undefined
   ) => {
     return newValue
-      ? dateToDateTimeLocal(newValue)
+      ? //@ts-ignore
+        dateToDateTimeLocal(newValue)
       : courseData && currentValue
       ? dateToDateTimeLocal(currentValue)
       : '';
@@ -121,230 +128,287 @@ export default function CourseForm({
   };
 
   return (
-    <Container>
-      <Box
-        sx={{
-          p: 4,
-          mb: 4,
-          borderRadius: 2,
-          backgroundColor: palette.surface.main,
-          boxShadow: 8,
-        }}
-      >
-        <Typography
-          variant="h2"
-          color={palette.black.main}
-          textAlign="center"
-          marginBottom={1}
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Container>
+        <Box
+          sx={{
+            p: 4,
+            mb: 4,
+            borderRadius: 2,
+            backgroundColor: palette.surface.main,
+            boxShadow: 8,
+          }}
         >
-          {!isEditMode ? 'Add New Course' : 'Edit Course Details'}
-        </Typography>
-        <form
-          id="courseForm"
-          style={{ display: 'flex', flexDirection: 'column', gap: 10 }}
-          onSubmit={handleSubmit(submitForm)}
-        >
-          <InputLabel htmlFor="courseFormName">
-            {t('CourseForm.name')}
-          </InputLabel>
-          <Input
-            {...register('name')}
-            color="secondary"
-            id="courseFormName"
-            data-testid="courseFormName"
-            error={!!errors.name}
-            autoComplete="off"
-          />
-          <FormFieldError error={errors.name} />
-
-          <InputLabel htmlFor="courseFormDescription">
-            {t('CourseForm.description')}
-          </InputLabel>
-          <Controller
-            data-testid="courseFormDescription"
-            name="description"
-            control={control}
-            defaultValue=""
-            render={({ field: { onChange, value } }) => (
-              <RichTextEditor lang={lang} value={value} onChange={onChange} />
-            )}
-          />
-          <FormFieldError error={errors.description} />
-
-          <Controller
-            name="tags"
-            control={control}
-            defaultValue={[]}
-            render={({ field }) => {
-              return (
-                <>
-                  <InputLabel htmlFor="tagSelection">
-                    {t('CourseForm.tags')}
-                  </InputLabel>
-                  <Select
-                    {...field}
-                    id="tagSelection"
-                    color="secondary"
-                    multiple
-                    renderValue={(field) => (
-                      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                        {field.map((tag, idx) => (
-                          <Chip
-                            key={idx}
-                            label={tag}
-                            variant="outlined"
-                            sx={{
-                              backgroundColor: palette.surface.light,
-                              borderColor: palette.black.light,
-                            }}
-                          />
-                        ))}
-                      </Box>
-                    )}
-                  >
-                    {tags.map((tag) => (
-                      <MenuItem
-                        key={tag.id}
-                        value={tag.name}
-                        divider
-                        sx={{
-                          '&.Mui-selected': {
-                            backgroundColor: palette.surface.main,
-                          },
-                          '&.Mui-selected.Mui-focusVisible': {
-                            backgroundColor: palette.surface.dark,
-                          },
-                          '&:hover': {
-                            backgroundColor: palette.surface.light,
-                          },
-                          '&.Mui-selected:hover': {
-                            backgroundColor: palette.surface.main,
-                          },
-                        }}
-                      >
-                        {tag.name}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </>
-              );
-            }}
-          />
-          <InputLabel htmlFor="courseFormStartDate">
-            {t('CourseForm.startDate')}
-          </InputLabel>
-          <Input
-            {...register('startDate')}
-            color="secondary"
-            defaultValue={
-              courseData ? dateToDateTimeLocal(courseData.startDate) : ''
-            }
-            id="courseFormStartDate"
-            type="datetime-local"
-            error={!!errors.startDate}
-            inputProps={{
-              'data-testid': 'courseFormStartDate',
-            }}
-          />
-          <FormFieldError error={errors.startDate} />
-          <InputLabel htmlFor="courseFormEndDate">
-            {t('CourseForm.endDate')}
-          </InputLabel>
-          <Input
-            {...register('endDate')}
-            color="secondary"
-            defaultValue={
-              courseData ? dateToDateTimeLocal(courseData.endDate) : ''
-            }
-            id="courseFormEndDate"
-            type="datetime-local"
-            error={!!errors.endDate}
-            inputProps={{
-              'data-testid': 'courseFormEndDate',
-            }}
-          />
-          <FormFieldError error={errors.endDate} />
-
-          <InputLabel htmlFor="courseFormLastEnrollDate">
-            {t('CourseForm.lastEnrollDate')}
-          </InputLabel>
-          <Input
-            {...register('lastEnrollDate')}
-            color="secondary"
-            value={updateValue(lastEnrollDate, courseData?.lastEnrollDate)}
-            onChange={(e) => {
-              const selectedDate = e.target.value;
-              selectedDate === ''
-                ? setLastEnrollDate('')
-                : enrollDatehasData(new Date(selectedDate));
-            }}
-            id="courseFormLastEnrollDate"
-            type="datetime-local"
-            error={!!errors.lastEnrollDate}
-            inputProps={{
-              'data-testid': 'courseFormLastEnrollDate',
-            }}
-          />
-          <FormFieldError error={errors.lastEnrollDate} />
-
-          <InputLabel htmlFor="courseFormLastCancelDate">
-            {t('CourseForm.lastCancelDate')}
-          </InputLabel>
-          <Input
-            {...register('lastCancelDate')}
-            color="secondary"
-            value={updateValue(lastCancelDate, courseData?.lastCancelDate)}
-            onChange={(e) => {
-              const selectedDate = e.target.value;
-              selectedDate === ''
-                ? setLastCancelDate('')
-                : cancellDatehasData(new Date(selectedDate));
-            }}
-            id="courseFormLastCancelDate"
-            type="datetime-local"
-            error={!!errors.lastCancelDate}
-            inputProps={{
-              'data-testid': 'courseFormLastCancelDate',
-            }}
-          />
-          <FormFieldError error={errors.lastCancelDate} />
-
-          <InputLabel htmlFor="courseFormMaxStudents">
-            {t('CourseForm.maxStudents')}
-          </InputLabel>
-          <Input
-            {...register('maxStudents', {
-              setValueAs: (value) => Number(value),
-            })}
-            color="secondary"
-            id="courseFormMaxStudents"
-            type="number"
-            error={!!errors.maxStudents}
-            inputProps={{
-              min: 1,
-              'data-testid': 'courseFormMaxStudents',
-            }}
-          />
-          <FormFieldError error={errors.maxStudents} />
-          <Button
-            type="submit"
-            disabled={isSubmitting}
-            sx={{
-              display: 'block',
-              margin: 'auto',
-              mt: 2,
-              color: palette.white.main,
-              backgroundColor: palette.secondary.main,
-              '&:hover': {
-                backgroundColor: palette.secondary.light,
-              },
-            }}
-            data-testid="courseFormSubmit"
+          <Typography
+            variant="h2"
+            color={palette.black.main}
+            textAlign="center"
+            marginBottom={1}
           >
-            {isEditMode ? 'Update' : 'Submit'}
-          </Button>
-        </form>
-      </Box>
-    </Container>
+            {!isEditMode ? 'Add New Course' : 'Edit Course Details'}
+          </Typography>
+          <form
+            id="courseForm"
+            style={{ display: 'flex', flexDirection: 'column', gap: 10 }}
+            onSubmit={handleSubmit(submitForm)}
+          >
+            <InputLabel htmlFor="courseFormName">
+              {t('CourseForm.name')}
+            </InputLabel>
+            <Input
+              {...register('name')}
+              color="secondary"
+              id="courseFormName"
+              data-testid="courseFormName"
+              error={!!errors.name}
+              autoComplete="off"
+            />
+            <FormFieldError error={errors.name} />
+
+            <InputLabel htmlFor="courseFormDescription">
+              {t('CourseForm.description')}
+            </InputLabel>
+            <Controller
+              data-testid="courseFormDescription"
+              name="description"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value } }) => (
+                <RichTextEditor lang={lang} value={value} onChange={onChange} />
+              )}
+            />
+            <FormFieldError error={errors.description} />
+
+            <Controller
+              name="tags"
+              control={control}
+              defaultValue={[]}
+              render={({ field }) => {
+                return (
+                  <>
+                    <InputLabel htmlFor="tagSelection">
+                      {t('CourseForm.tags')}
+                    </InputLabel>
+                    <Select
+                      {...field}
+                      id="tagSelection"
+                      color="secondary"
+                      multiple
+                      renderValue={(field) => (
+                        <Box
+                          sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}
+                        >
+                          {field.map((tag, idx) => (
+                            <Chip
+                              key={idx}
+                              label={tag}
+                              variant="outlined"
+                              sx={{
+                                backgroundColor: palette.surface.light,
+                                borderColor: palette.black.light,
+                              }}
+                            />
+                          ))}
+                        </Box>
+                      )}
+                    >
+                      {tags.map((tag) => (
+                        <MenuItem
+                          key={tag.id}
+                          value={tag.name}
+                          divider
+                          sx={{
+                            '&.Mui-selected': {
+                              backgroundColor: palette.surface.main,
+                            },
+                            '&.Mui-selected.Mui-focusVisible': {
+                              backgroundColor: palette.surface.dark,
+                            },
+                            '&:hover': {
+                              backgroundColor: palette.surface.light,
+                            },
+                            '&.Mui-selected:hover': {
+                              backgroundColor: palette.surface.main,
+                            },
+                          }}
+                        >
+                          {tag.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </>
+                );
+              }}
+            />
+            <InputLabel htmlFor="courseFormStartDate">
+              {t('CourseForm.endDate')}
+            </InputLabel>
+            <DateTimePicker
+              {...register('startDate')}
+              value={dayjs(updateValue(startDate, courseData?.startDate))}
+              onChange={(value) => {
+                if (value === null) {
+                  setStartDate('');
+                } else {
+                  const selectedDate = dayjs(value).toDate();
+                  setStartDate(selectedDate);
+                  //@ts-ignore
+                  setValue('startDate', dateToDateTimeLocal(selectedDate));
+                }
+              }}
+              timeSteps={{ minutes: 1 }}
+              slotProps={{
+                textField: { variant: 'outlined' },
+                actionBar: {
+                  actions: ['accept', 'clear'],
+                },
+              }}
+              //@ts-ignore
+              id="courseFormStartDate"
+              error={!!errors.startDate}
+              inputProps={{
+                'data-testid': 'courseFormStartDate',
+              }}
+            />
+            <FormFieldError error={errors.startDate} />
+            <InputLabel htmlFor="courseFormEndDate">
+              {t('CourseForm.endDate')}
+            </InputLabel>
+            <DateTimePicker
+              {...register('endDate')}
+              value={dayjs(updateValue(endDate, courseData?.endDate))}
+              onChange={(value) => {
+                if (value === null) {
+                  setEndDate('');
+                } else {
+                  const selectedDate = dayjs(value).toDate();
+                  setEndDate(selectedDate);
+                  //@ts-ignore
+                  setValue('endDate', dateToDateTimeLocal(selectedDate));
+                }
+              }}
+              timeSteps={{ minutes: 1 }}
+              slotProps={{
+                textField: { variant: 'outlined' },
+                actionBar: {
+                  actions: ['accept', 'clear'],
+                },
+              }}
+              //@ts-ignore
+              id="courseFormEndDate"
+              error={!!errors.endDate}
+              inputProps={{
+                'data-testid': 'courseFormEndDate',
+              }}
+            />
+            <FormFieldError error={errors.endDate} />
+
+            <InputLabel htmlFor="courseFormLastEnrollDate">
+              {t('CourseForm.lastEnrollDate')}
+            </InputLabel>
+            <DateTimePicker
+              {...register('lastEnrollDate')}
+              data-testid="courseFormLastEnrollDate"
+              value={dayjs(
+                updateValue(lastEnrollDate, courseData?.lastEnrollDate)
+              )}
+              onChange={(value) => {
+                if (value === null) {
+                  setLastEnrollDate('');
+                } else {
+                  const selectedDate = dayjs(value).toDate();
+                  enrollDatehasData(new Date(selectedDate));
+                  //@ts-ignore
+                  setValue('lastEnrollDate', dateToDateTimeLocal(selectedDate));
+                }
+              }}
+              timeSteps={{ minutes: 1 }}
+              slotProps={{
+                textField: { variant: 'outlined' },
+                actionBar: {
+                  actions: ['accept', 'clear'],
+                },
+              }}
+              //@ts-ignore
+              id="courseFormLastEnrollDate"
+              error={!!errors.lastEnrollDate}
+              inputProps={{
+                'data-testid': 'courseFormLastEnrollDate',
+              }}
+            />
+            <FormFieldError error={errors.lastEnrollDate} />
+
+            <InputLabel htmlFor="courseFormLastCancelDate">
+              {t('CourseForm.lastCancelDate')}
+            </InputLabel>
+            <DateTimePicker
+              {...register('lastCancelDate')}
+              value={dayjs(
+                updateValue(lastCancelDate, courseData?.lastCancelDate)
+              )}
+              onChange={(value) => {
+                if (value === null) {
+                  setLastCancelDate('');
+                } else {
+                  const selectedDate = dayjs(value).toDate();
+                  cancellDatehasData(new Date(selectedDate));
+                  //@ts-ignore
+                  setValue('lastCancelDate', dateToDateTimeLocal(selectedDate));
+                }
+              }}
+              timeSteps={{ minutes: 1 }}
+              slotProps={{
+                textField: { variant: 'outlined' },
+                actionBar: {
+                  actions: ['accept', 'clear'],
+                },
+              }}
+              //@ts-ignore
+              id="courseFormLastCancelDate"
+              error={!!errors.lastEnrollDate}
+              inputProps={{
+                'data-testid': 'courseFormLastCancelDate',
+              }}
+            />
+            <FormFieldError error={errors.lastCancelDate} />
+
+            <InputLabel htmlFor="courseFormMaxStudents">
+              {t('CourseForm.maxStudents')}
+            </InputLabel>
+            <Input
+              {...register('maxStudents', {
+                setValueAs: (value) => Number(value),
+              })}
+              color="secondary"
+              id="courseFormMaxStudents"
+              type="number"
+              error={!!errors.maxStudents}
+              inputProps={{
+                min: 1,
+                'data-testid': 'courseFormMaxStudents',
+              }}
+            />
+            <FormFieldError error={errors.maxStudents} />
+            <Button
+              type="submit"
+              disabled={isSubmitting}
+              sx={{
+                display: 'block',
+                margin: 'auto',
+                mt: 2,
+                color: palette.white.main,
+                backgroundColor: palette.secondary.main,
+                '&:hover': {
+                  backgroundColor: palette.secondary.light,
+                },
+              }}
+              data-testid="courseFormSubmit"
+            >
+              {isEditMode ? 'Update' : 'Submit'}
+            </Button>
+          </form>
+        </Box>
+      </Container>
+    </LocalizationProvider>
   );
 }
