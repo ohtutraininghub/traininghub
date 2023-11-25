56b9d9792a9eb61577ed45d21494c406cf194816
Mikael Moilanen <mikael.moilanen@gmail.com>
2023-11-25 19:28:47 +0200

    Add filtering to user list view

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -8,9 +8,11 @@ import { Users } from '@/lib/prisma/users';
 import { handleCommonErrors } from '@/lib/response/errorUtil';
 import { update } from '@/lib/response/fetchUtil';
 import { MessageType } from '@/lib/response/responseUtil';
+import { FilterAlt } from '@mui/icons-material';
 
 import {
   Box,
+  IconButton,
   MenuItem,
   Select,
   Table,
@@ -21,11 +23,13 @@ import {
   TablePagination,
   TableRow,
   TableSortLabel,
+  TextField,
+  Tooltip,
   Typography,
 } from '@mui/material';
 import { $Enums, User } from '@prisma/client';
 import { useRouter } from 'next/navigation';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 interface Props extends DictProps {
   users: Users;
@@ -59,6 +63,17 @@ function getComparator<Key extends keyof any>(
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
+function checkFilterConditions(
+  user: User,
+  headerFilters: [string, string, string]
+) {
+  return (
+    user.name?.toLowerCase().indexOf(headerFilters[0].toLowerCase()) !== -1 &&
+    user.email?.toLowerCase().indexOf(headerFilters[1].toLowerCase()) !== -1 &&
+    user.role.toLowerCase().indexOf(headerFilters[2].toLowerCase()) !== -1
+  );
+}
+
 export default function UserList({ lang, users }: Props) {
   const { t } = useTranslation(lang, 'admin');
   const { notify } = useMessage();
@@ -68,8 +83,35 @@ export default function UserList({ lang, users }: Props) {
   const [visibleRowsPerPage, setVisibleRowsPerPage] = useState(5);
   const [orderBy, setOrderBy] = useState<TableHeader>('name');
   const [order, setOrder] = useState<Order>('asc');
-  // list of users used to keep track of user right changes before saving
-  const [tempUsers, setTempUsers] = useState<User[]>(users);
+  // list of users used to keep track of user role changes before saving
+  const [filteredUsers, setFilteredUsers] = useState<User[]>(users);
+  const [displayFilterRow, setDisplayFilterRow] = useState(false);
+  const [headerFilters, setHeaderFilters] = useState<[string, string, string]>([
+    '',
+    '',
+    '',
+  ]);
+
+  useEffect(() => {
+    setFilteredUsers((prevFilteredUsers) =>
+      users
+        .map(
+          (user) => prevFilteredUsers.find((usr) => usr.id === user.id) ?? user
+        )
+        .filter((user) => checkFilterConditions(user, headerFilters))
+    );
+  }, [users, headerFilters]);
+
+  const visibleUsers = useMemo(
+    () =>
+      filteredUsers
+        .sort(getComparator(order, orderBy))
+        .slice(
+          page * visibleRowsPerPage,
+          page * visibleRowsPerPage + visibleRowsPerPage
+        ),
+    [order, orderBy, page, visibleRowsPerPage, filteredUsers]
+  );
 
   function handleRequestSort(
     _event: React.MouseEvent<unknown>,
@@ -102,27 +144,13 @@ export default function UserList({ lang, users }: Props) {
     }
   }
 
-  const tableHeaders: TableHeader[] = [
-    'name',
-    'email',
-    'emailVerified',
-    'role',
-  ];
-
-  const visibleUsers = useMemo(
-    () =>
-      users
-        .sort(getComparator(order, orderBy))
-        .slice(
-          page * visibleRowsPerPage,
-          page * visibleRowsPerPage + visibleRowsPerPage
-        ),
-    [order, orderBy, page, users, visibleRowsPerPage]
-  );
+  const tableHeaders: TableHeader[] = ['name', 'email', 'role'];
 
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * visibleRowsPerPage - users.length) : 0;
+    page > 0
+      ? Math.max(0, (1 + page) * visibleRowsPerPage - filteredUsers.length)
+      : 0;
 
   return (
     <div
@@ -132,21 +160,38 @@ export default function UserList({ lang, users }: Props) {
         padding: '1rem',
       }}
     >
-      <Box sx={{ paddingBottom: '1rem' }}>
+      <Box sx={{ display: 'flex', paddingBottom: '1rem' }}>
         <Typography variant="h4" component="h2">
           {t('EditUsers.label')}
         </Typography>
+        <Tooltip
+          title={
+            displayFilterRow
+              ? t('EditUsers.filterHideLabel')
+              : t('EditUsers.filterToggleLabel')
+          }
+        >
+          <IconButton
+            sx={{ marginLeft: 'auto' }}
+            onClick={() => {
+              setDisplayFilterRow((prev) => !prev);
+              setHeaderFilters(['', '', '']);
+            }}
+          >
+            <FilterAlt />
+          </IconButton>
+        </Tooltip>
       </Box>
 
       <TableContainer>
         <Table aria-label="user table">
           <TableHead>
             <TableRow>
-              {tableHeaders.map((headerKey) => (
+              {tableHeaders.map((headerKey, keyIndex) => (
                 <TableCell
+                  sx={{ width: `${100 / tableHeaders.length}%` }}
                   sortDirection={orderBy === headerKey ? 'asc' : 'desc'}
                   key={headerKey}
-                  align={headerKey === 'emailVerified' ? 'center' : 'left'}
                 >
                   <TableSortLabel
                     onClick={(event) => handleRequestSort(event, headerKey)}
@@ -156,6 +201,34 @@ export default function UserList({ lang, users }: Props) {
                     {' '}
                     {t(`EditUsers.tableHeaders.${headerKey}`)}
                   </TableSortLabel>
+                  {displayFilterRow && (
+                    <Tooltip
+                      placement="bottom-start"
+                      title={t('EditUsers.searchTooltip', {
+                        headerValue: headerKey,
+                      })}
+                    >
+                      <TextField
+                        onChange={(event) => {
+                          setHeaderFilters(
+                            (prevFilters) =>
+                              prevFilters.map((filter, index) =>
+                                index === keyIndex ? event.target.value : filter
+                              ) as typeof headerFilters
+                          );
+                        }}
+                        sx={{
+                          marginTop: '0.15rem',
+                          display: 'block',
+                          '& .MuiInputBase-input': {
+                            padding: '0.35rem',
+                            fontSize: '0.75rem',
+                          },
+                        }}
+                        placeholder={t(`EditUsers.tableHeaders.${headerKey}`)}
+                      />
+                    </Tooltip>
+                  )}
                 </TableCell>
               ))}
             </TableRow>
@@ -169,11 +242,7 @@ export default function UserList({ lang, users }: Props) {
                 <TableCell component="th" scope="row">
                   {user?.email ?? '-'}
                 </TableCell>
-                <TableCell align="center" component="th" scope="row">
-                  {user.emailVerified
-                    ? JSON.stringify(user.emailVerified)
-                    : '-'}
-                </TableCell>
+
                 <TableCell
                   component="th"
                   scope="row"
@@ -188,7 +257,7 @@ export default function UserList({ lang, users }: Props) {
                     }}
                     fullWidth
                     onChange={(event) => {
-                      setTempUsers((prevUsers) =>
+                      setFilteredUsers((prevUsers) =>
                         prevUsers.map((prevUser, index) =>
                           index === page * visibleRowsPerPage + userIndex
                             ? {
@@ -200,7 +269,7 @@ export default function UserList({ lang, users }: Props) {
                       );
                     }}
                     value={
-                      tempUsers[page * visibleRowsPerPage + userIndex].role
+                      filteredUsers[page * visibleRowsPerPage + userIndex].role
                     }
                   >
                     {Object.keys($Enums.Role).map((enumKey) => (
@@ -213,16 +282,21 @@ export default function UserList({ lang, users }: Props) {
                   </Select>
 
                   {user.role !==
-                    tempUsers.find((tempUser) => tempUser.id === user.id)
+                    users.find((filteredUser) => filteredUser.id === user.id)
                       ?.role && (
                     <SmallConfirmCard
                       confirmTooltip={t('EditUsers.confirmCard.confirm')}
                       cancelTooltip={t('EditUsers.confirmCard.cancel')}
                       onCancel={() =>
-                        setTempUsers((prevUsers) =>
+                        setFilteredUsers((prevUsers) =>
                           prevUsers.map((prevUser, index) =>
                             index === page * visibleRowsPerPage + userIndex
-                              ? { ...user, role: user.role }
+                              ? {
+                                  ...user,
+                                  role:
+                                    users.find((u) => u.id === prevUser.id)
+                                      ?.role ?? prevUser.role,
+                                }
                               : prevUser
                           )
                         )
@@ -230,7 +304,8 @@ export default function UserList({ lang, users }: Props) {
                       onSubmit={async () => {
                         await handleUserRoleChange(
                           user.id,
-                          tempUsers[page * visibleRowsPerPage + userIndex].role
+                          filteredUsers[page * visibleRowsPerPage + userIndex]
+                            .role
                         );
                       }}
                     />
@@ -253,7 +328,7 @@ export default function UserList({ lang, users }: Props) {
       <TablePagination
         rowsPerPageOptions={[5, 10, 25]}
         component="div"
-        count={users.length}
+        count={filteredUsers.length}
         rowsPerPage={visibleRowsPerPage}
         page={page}
         onPageChange={handleChangePage}
