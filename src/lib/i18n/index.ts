331f85d6b69941f46e84ed7990058632251f32e1
tommivk <tommikokk@gmail.com>
2023-11-30 12:49:13 +0200

    fix array type

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -33,8 +33,7 @@ export function getLocale(request: NextRequest): string | undefined {
   const negotiatorHeaders: Record<string, string> = {};
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
-  // @ts-ignore locales are readonly
-  const locales: string[] = i18n.locales;
+  const locales = [...i18n.locales];
 
   // Use negotiator and intl-localematcher to get best locale
   let languages = new Negotiator({ headers: negotiatorHeaders }).languages(
