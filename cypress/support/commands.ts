94d074e98b95dd88ab4ff8622ec7f0da30583175
vaisajuh <juha.vaisanen@gmail.com>
2023-12-02 01:29:05 +0200

    Fix Cypress tests

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -12,40 +12,28 @@ Cypress.Commands.add('setDate', (element, value, day, hours, minutes) => {
   cy.wait(2000);
   cy.wrap(element).click();
   if (value === 'currentMonth') {
-    cy.contains(day).click();
-    cy.get(`[aria-label="${hours} hours"]`)
-      .invoke('css', 'pointer-events', 'auto')
-      .click();
-    cy.get(`[aria-label="${minutes} minutes"]`)
-      .invoke('css', 'pointer-events', 'auto')
-      .click();
-    cy.get('[aria-label="PM"]').invoke('css', 'pointer-events', 'auto').click();
-    cy.contains('OK').click();
+    cy.contains(day).click({ force: true });
+    cy.get(`[aria-label="${hours} hours"]`).click({ force: true });
+    cy.get(`[aria-label="${minutes} minutes"]`).click({ force: true });
+    cy.get('[aria-label="PM"]').click({ force: true });
+    cy.contains('OK').click({ force: true });
   }
 
   if (value === 'nextMonth') {
-    cy.get('[aria-label="Next month"]').click();
-    cy.contains(day).click();
-    cy.get(`[aria-label="${hours} hours"]`)
-      .invoke('css', 'pointer-events', 'auto')
-      .click();
-    cy.get(`[aria-label="${minutes} minutes"]`)
-      .invoke('css', 'pointer-events', 'auto')
-      .click();
-    cy.get('[aria-label="PM"]').invoke('css', 'pointer-events', 'auto').click();
-    cy.contains('OK').click();
+    cy.get('[aria-label="Next month"]').click({ force: true });
+    cy.contains(day).click({ force: true });
+    cy.get(`[aria-label="${hours} hours"]`).click();
+    cy.get(`[aria-label="${minutes} minutes"]`).click();
+    cy.get('[aria-label="PM"]').click({ force: true });
+    cy.contains('OK').click({ force: true });
   }
 
   if (value === 'previousMonth') {
-    cy.get('[aria-label="Previous month"]').click();
-    cy.contains(day).click();
-    cy.get(`[aria-label="${hours} hours"]`)
-      .invoke('css', 'pointer-events', 'auto')
-      .click();
-    cy.get(`[aria-label="${minutes} minutes"]`)
-      .invoke('css', 'pointer-events', 'auto')
-      .click();
-    cy.get('[aria-label="PM"]').invoke('css', 'pointer-events', 'auto').click();
-    cy.contains('OK').click();
+    cy.get('[aria-label="Previous month"]').click({ force: true });
+    cy.contains(day).click({ force: true });
+    cy.get(`[aria-label="${hours} hours"]`).click({ force: true });
+    cy.get(`[aria-label="${minutes} minutes"]`).click({ force: true });
+    cy.get('[aria-label="PM"]').click({ force: true });
+    cy.contains('OK').click({ force: true });
   }
 });
