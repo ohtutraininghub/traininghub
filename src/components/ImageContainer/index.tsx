ac4f78d442ea6c0af0c63832c116d10e1ccad820
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-12-10 23:06:57 +0200

    course card visual improvements

--- src/components/ImageContainer/index.tsx
+++ src/components/ImageContainer/index.tsx
@@ -23,7 +23,6 @@ export function ImageContainer({
       data-testid="courseImage"
       style={{
         alignSelf: 'center',
-        margin: '1rem 0 1rem 0',
         border: withBorder
           ? `1px solid ${theme.palette.coverBlue.dark}`
           : 'none',
