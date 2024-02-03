800090a2987de739a17e18ef3136264e2c0f8a1f
akskokki <akseli.kokki@gmail.com>
2024-02-03 11:04:41 +0200

    Pull template delete api from branch for task 312

---
+++ src/app/api/template/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest } from 'next/server';
+import {
+  StatusCodeType,
+  errorResponse,
+  successResponse,
+} from '@/lib/response/responseUtil';
+import { prisma } from '@/lib/prisma';
+import { handleCommonErrors } from '@/lib/response/errorUtil';
+import { getServerAuthSession } from '@/lib/auth';
+import { isTrainerOrAdmin } from '@/lib/auth-utils';
+import { translator } from '@/lib/i18n';
+import { templateDeleteSchema } from '@/lib/zod/templates';
+
+export async function DELETE(request: NextRequest) {
+  console.log('template delete');
+  try {
+    const { t } = await translator('api');
+    const { user } = await getServerAuthSession();
+    const reqData = await request.json();
+    const template = templateDeleteSchema.parse(reqData);
+
+    const template_exists = await prisma.template.findFirst({
+      where: { id: template.templateId },
+    });
+
+    if (!template_exists) {
+      return errorResponse({
+        message: t('Common.courseNotFound'),
+        statusCode: StatusCodeType.NOT_FOUND,
+      });
+    }
+
+    await prisma.template.delete({
+      where: {
+        id: template.templateId,
+      },
+    });
+
+    if (!isTrainerOrAdmin(user)) {
+      return errorResponse({
+        message: t('Common.forbidden'),
+        statusCode: StatusCodeType.FORBIDDEN,
+      });
+    }
+
+    return successResponse({
+      message: t('Templates.templateDeleted'),
+      statusCode: StatusCodeType.OK,
+    });
+  } catch (error) {
+    return await handleCommonErrors(error);
+  }
+}
---
+++ src/lib/zod/templates.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const templateSchema = z.object({
+  name: z.string(),
+  description: z.string().optional(),
+});
+
+export const templateSchemaWithId = templateSchema.extend({
+  id: z.string().min(1),
+});
+
+export const templateDeleteSchema = z
+  .object({
+    templateId: z.string().cuid(),
+  })
+  .strict();
