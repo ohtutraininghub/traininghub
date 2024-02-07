472d8773383ad1c232de5b470c66f5175c788896
rauhja <82282638+rauhja@users.noreply.github.com>
2024-02-07 13:48:29 +0200

    Add delete event and find entry by course id

--- src/lib/prisma/calendar.ts
+++ src/lib/prisma/calendar.ts
@@ -44,6 +44,17 @@ export const findGoogleCalendarEntry = async (
   });
 };
 
+export const findGoogleCalendarEntryByCourseId = async (courseId: string) => {
+  return await prisma.calendar.findMany({
+    where: {
+      courseId: courseId,
+    },
+    select: {
+      googleEventId: true,
+    },
+  });
+};
+
 export const getUsersWithGoogleCalendar = async (course: Course) => {
   return await prisma.calendar.findMany({
     where: {
@@ -52,6 +63,14 @@ export const getUsersWithGoogleCalendar = async (course: Course) => {
     select: {
       userId: true,
       googleEventId: true,
+    },
+  });
+};
+
+export const deleteGoogleCalendarEntryByCourseId = async (courseId: string) => {
+  return await prisma.calendar.deleteMany({
+    where: {
+      courseId: courseId,
     },
   });
 };
