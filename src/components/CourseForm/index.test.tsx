aff44f73a7b9f83e1cbc83473de4386926f9a6c2
erjav <erja.vaskivuori@helsinki.fi>
2024-02-15 16:59:01 +0200

    A bit of refactoring and new tests for CourseForm's edit and create modes

--- src/components/CourseForm/index.test.tsx
+++ src/components/CourseForm/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { screen, waitFor, fireEvent } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderWithTheme } from '@/lib/test-utils';
 import CourseForm from '.';
@@ -128,7 +128,66 @@ describe('Course Form Course Create Tests', () => {
     });
   });
 
-  it('Form fields can be filled with values in Create Mode', async () => {
+  it('Form is submitted with the optional fields empty in Create Mode', async () => {
+    renderWithTheme(
+      <CourseForm lang="en" tags={course.tags} templates={[template]} />
+    );
+
+    // find input fields
+    const nameInput = screen.getByTestId('courseFormName') as HTMLInputElement;
+    const descriptionInput = screen.getByTestId('courseFormDescription');
+    const maxStudentsInput = screen.getByTestId(
+      'courseFormMaxStudents'
+    ) as HTMLInputElement;
+    const tagsInput = screen.getByRole('combobox', {
+      name: /CourseForm.tags/i,
+    });
+
+    // fill in the input fields
+    await user.type(nameInput, course.name);
+    await user.type(descriptionInput, course.description);
+    // clear the default value before typing
+    await userEvent.clear(maxStudentsInput);
+    await user.type(maxStudentsInput, course.maxStudents.toString());
+
+    await user.click(tagsInput);
+
+    course.tags.forEach((tag) => {
+      const option = screen.getByText(tag.name);
+      user.click(option);
+    });
+
+    // set the date and time input values
+    const startDateInput = screen.getByTestId(
+      'courseFormStartDate'
+    ) as HTMLInputElement;
+    setNativeValue(startDateInput, courseStart.toISOString().slice(0, 16));
+
+    const endDateInput = screen.getByTestId(
+      'courseFormEndDate'
+    ) as HTMLInputElement;
+    setNativeValue(endDateInput, courseEnd.toISOString().slice(0, 16));
+
+    const submitButton = screen.getByTestId('courseFormSubmit');
+    await userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(mockFetch).toBeCalledWith('/api/course', {
+        name: course.name,
+        description: course.description,
+        tags: course.tags.map((tag) => tag.name),
+        summary: '',
+        image: '',
+        startDate: new Date(course.startDate.toISOString().slice(0, 16)),
+        endDate: new Date(course.endDate.toISOString().slice(0, 16)),
+        lastEnrollDate: null,
+        lastCancelDate: null,
+        maxStudents: course.maxStudents,
+      });
+    });
+  });
+
+  it('All form fields can be filled with values in Create Mode', async () => {
     renderWithTheme(
       <CourseForm lang="en" tags={course.tags} templates={[template]} />
     );
@@ -165,6 +224,7 @@ describe('Course Form Course Create Tests', () => {
       user.click(option);
     });
 
+    // set the date and time input values
     const startDateInput = screen.getByTestId(
       'courseFormStartDate'
     ) as HTMLInputElement;
@@ -175,11 +235,33 @@ describe('Course Form Course Create Tests', () => {
     ) as HTMLInputElement;
     setNativeValue(endDateInput, courseEnd.toISOString().slice(0, 16));
 
+    const lastEnrollDateInput = screen.getByTestId(
+      'courseFormLastEnrollDate'
+    ) as HTMLInputElement;
+    setNativeValue(
+      lastEnrollDateInput,
+      oneDayBeforeStart.toISOString().slice(0, 16)
+    );
+
+    const lastCancelDateInput = screen.getByTestId(
+      'courseFormLastCancelDate'
+    ) as HTMLInputElement;
+    setNativeValue(
+      lastCancelDateInput,
+      oneDayBeforeStart.toISOString().slice(0, 16)
+    );
+
     // check if the input fields have the correct values
     expect(startDateInput.value).toBe(
       course.startDate.toISOString().slice(0, 16)
     );
     expect(endDateInput.value).toBe(course.endDate.toISOString().slice(0, 16));
+    expect(lastEnrollDateInput.value).toBe(
+      oneDayBeforeStart.toISOString().slice(0, 16)
+    );
+    expect(lastCancelDateInput.value).toBe(
+      oneDayBeforeStart.toISOString().slice(0, 16)
+    );
     expect(nameInput.value).toBe(course.name);
     expect(descriptionInput).toHaveTextContent(course.description);
     expect(summaryInput.value).toBe(course.summary);
@@ -237,12 +319,22 @@ describe('Course Form Course Create Tests', () => {
       'courseFormEndDate'
     ) as HTMLInputElement;
     setNativeValue(endDateInput, courseEnd.toISOString().slice(0, 16));
-    expect(endDateInput.value).toBe(course.endDate.toISOString().slice(0, 16));
-    expect(nameInput.value).toBe(course.name);
-    expect(descriptionInput).toHaveTextContent(course.description);
-    expect(summaryInput.value).toBe(course.summary);
-    expect(maxStudentsInput.value).toBe(course.maxStudents.toString());
-    expect(imageInput.value).toBe(course.image);
+
+    const lastEnrollDateInput = screen.getByTestId(
+      'courseFormLastEnrollDate'
+    ) as HTMLInputElement;
+    setNativeValue(
+      lastEnrollDateInput,
+      oneDayBeforeStart.toISOString().slice(0, 16)
+    );
+
+    const lastCancelDateInput = screen.getByTestId(
+      'courseFormLastCancelDate'
+    ) as HTMLInputElement;
+    setNativeValue(
+      lastCancelDateInput,
+      oneDayBeforeStart.toISOString().slice(0, 16)
+    );
 
     const submitButton = screen.getByTestId('courseFormSubmit');
     await userEvent.click(submitButton);
@@ -256,8 +348,8 @@ describe('Course Form Course Create Tests', () => {
         image: course.image,
         startDate: new Date(course.startDate.toISOString().slice(0, 16)),
         endDate: new Date(course.endDate.toISOString().slice(0, 16)),
-        lastEnrollDate: null,
-        lastCancelDate: null,
+        lastEnrollDate: new Date(oneDayBeforeStart.toISOString().slice(0, 16)),
+        lastCancelDate: new Date(oneDayBeforeStart.toISOString().slice(0, 16)),
         maxStudents: Number(course.maxStudents),
       });
     });
@@ -282,9 +374,7 @@ describe('Course Form Course Create Tests', () => {
   });
 
   it('updates the value of the field when a template is selected', async () => {
-    const { container } = renderWithTheme(
-      <CourseForm lang="en" tags={[]} templates={[template]} />
-    );
+    renderWithTheme(<CourseForm lang="en" tags={[]} templates={[template]} />);
     const select = screen.getByRole('combobox', {
       name: /Template.selectTemplate/i,
     });
@@ -369,17 +459,40 @@ describe('Course Form Course Edit Tests', () => {
   const oneDayBeforeStart = new Date();
 
   const template = {
-    id: '',
-    name: '',
-    description: '',
-    summary: '',
-    tags: [],
-    maxStudents: 0,
-    createdById: '',
-    image: '',
+    id: '1234',
+    name: 'New course',
+    description: 'A test course',
+    summary: 'All you ever wanted to know about testing!',
+    tags: [{ id: '1', name: 'Testing' }],
+    maxStudents: 55,
+    createdById: '30',
+    image: 'http://test-image.com',
   };
 
   it('Template dropdown is not displayed in Edit Mode', async () => {
+    const course = {
+      id: '1234',
+      createdById: '30',
+      name: 'New course',
+      description: 'A test course',
+      summary: 'All you ever wanted to know about testing!',
+      startDate: courseStart,
+      endDate: courseEnd,
+      lastEnrollDate: oneDayBeforeStart,
+      lastCancelDate: oneDayBeforeStart,
+      maxStudents: 55,
+      tags: [],
+      image: '',
+    };
+
+    renderWithTheme(
+      <CourseForm
+        lang="en"
+        tags={[]}
+        courseData={course}
+        templates={[template]}
+      />
+    );
     const selectTemplate = screen.queryByText(/Template.selectTemplate/i);
     expect(selectTemplate).toBeNull();
   });
@@ -400,7 +513,7 @@ describe('Course Form Course Edit Tests', () => {
       image: '',
     };
 
-    const { container } = renderWithTheme(
+    renderWithTheme(
       <CourseForm
         lang="en"
         tags={[]}
@@ -458,7 +571,7 @@ describe('Course Form Course Edit Tests', () => {
       image: '',
     };
 
-    const { container } = renderWithTheme(
+    renderWithTheme(
       <CourseForm
         lang="en"
         tags={[]}
@@ -573,6 +686,125 @@ describe('Course Form Course Edit Tests', () => {
         lastCancelDate: null,
         maxStudents: Number(course.maxStudents),
       });
+    });
+  });
+
+  it('Form is submitted with correct values in Edit Mode when values have been edited', async () => {
+    mockFetch.mockClear();
+    const user = userEvent.setup();
+
+    const course = {
+      id: '1234',
+      createdById: '30',
+      name: 'New course',
+      description: 'A test course',
+      summary: 'All you ever wanted to know about testing!',
+      startDate: courseStart,
+      endDate: courseEnd,
+      lastEnrollDate: oneDayBeforeStart,
+      lastCancelDate: oneDayBeforeStart,
+      maxStudents: 55,
+      tags: [],
+      image: '',
+    };
+    const tags = [
+      { id: '1', name: 'Testing' },
+      { id: '2', name: 'Git' },
+    ];
+
+    const editedCourse = {
+      id: '1234',
+      createdById: '30',
+      name: 'New edited course',
+      description: 'A test course which has been edited',
+      summary: 'Updated version of the course',
+      startDate: courseStart,
+      endDate: courseEnd,
+      lastEnrollDate: oneDayBeforeStart,
+      lastCancelDate: oneDayBeforeStart,
+      maxStudents: 40,
+      tags: [{ id: '2', name: 'Git' }],
+      image: 'http://test-image.com',
+    };
+
+    renderWithTheme(
+      <CourseForm lang="en" tags={tags} courseData={course} templates={[]} />
+    );
+
+    const name = screen.getByTestId('courseFormName') as HTMLInputElement;
+    await user.clear(name);
+    await user.type(name, editedCourse.name);
+
+    const description = screen.getByTestId('courseFormDescription');
+    await user.type(description, ' which has been edited');
+
+    const summary = screen.getByTestId('courseFormSummary') as HTMLInputElement;
+    await user.clear(summary);
+    await user.type(summary, editedCourse.summary);
+
+    const maxStudents = screen.getByTestId(
+      'courseFormMaxStudents'
+    ) as HTMLInputElement;
+    await user.clear(maxStudents);
+    await user.type(maxStudents, editedCourse.maxStudents.toString());
+
+    const image = screen.getByTestId('courseFormImage') as HTMLInputElement;
+    await user.type(image, editedCourse.image);
+
+    const tagsInput = screen.getByRole('combobox', {
+      name: /CourseForm.tags/i,
+    });
+    await user.click(tagsInput);
+    const option = screen.getByText(tags[1].name);
+    await user.click(option);
+
+    const submitButton = screen.getByTestId('courseFormSubmit');
+    await userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(mockFetch).toBeCalledWith('/api/course', {
+        ...editedCourse,
+        endDate: new Date(course.endDate),
+        startDate: new Date(course.startDate),
+        lastEnrollDate: new Date(course.lastEnrollDate),
+        lastCancelDate: new Date(course.lastCancelDate),
+        maxStudents: Number(editedCourse.maxStudents),
+        tags: [tags[1].name],
+      });
+    });
+  });
+
+  it('should not be possible to clear name field and submit', async () => {
+    mockFetch.mockClear();
+    const user = userEvent.setup();
+
+    const course = {
+      id: '1234',
+      createdById: '30',
+      name: 'New course',
+      description: 'A test course',
+      summary: 'All you ever wanted to know about testing!',
+      startDate: courseStart,
+      endDate: courseEnd,
+      lastEnrollDate: oneDayBeforeStart,
+      lastCancelDate: oneDayBeforeStart,
+      maxStudents: 55,
+      tags: [],
+      image: '',
+    };
+
+    renderWithTheme(
+      <CourseForm lang="en" tags={[]} courseData={course} templates={[]} />
+    );
+
+    const name = screen.getByTestId('courseFormName') as HTMLInputElement;
+    await user.clear(name);
+
+    const submitButton = screen.getByTestId('courseFormSubmit');
+    await userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(mockFetch).not.toBeCalled();
     });
   });
 });
