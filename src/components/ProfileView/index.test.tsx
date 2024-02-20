3c33a9bd83b01c2610783fbf52603609f0b3eb98
erjav <erja.vaskivuori@helsinki.fi>
2024-02-20 18:04:34 +0200

    Test rendering correct dropdowns for different user types

--- src/components/ProfileView/index.test.tsx
+++ src/components/ProfileView/index.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderWithTheme } from '@/lib/test-utils';
-import { User, Role } from '@prisma/client';
+import { Role } from '@prisma/client';
 import ProfileView from './index';
 import { useSession } from 'next-auth/react';
 
@@ -23,7 +22,14 @@ jest.mock('next-auth/react', () => ({
   useSession: jest.fn(),
 }));
 
-const adminUser: User = {
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn().mockImplementation(() => ({
+    push: jest.fn(),
+    refresh: jest.fn(),
+  })),
+}));
+
+const adminUser = {
   id: '123a',
   name: 'Ada Admin',
   email: 'admin@traininghub.org',
@@ -32,7 +38,7 @@ const adminUser: User = {
   role: Role.ADMIN,
 };
 
-const trainerUser: User = {
+const trainerUser = {
   id: '123b',
   name: 'Teddy Trainer',
   email: 'trainer@traininghub.org',
@@ -48,6 +54,7 @@ const template = {
   summary: 'All you ever wanted to know about testing!',
   tags: [{ id: '1', name: 'Testing' }],
   maxStudents: 55,
+  createdBy: trainerUser,
   createdById: trainerUser.id,
   image: 'http://test-image.com',
 };
@@ -124,7 +131,7 @@ const traineeUser = {
 };
 
 describe('ProfileView Tests', () => {
-  it('should render courses in the correct dropdowns', async () => {
+  it('should render correct dropdowns for trainee', async () => {
     (useSession as jest.Mock).mockReturnValue({
       data: {
         user: traineeUser,
@@ -156,8 +163,123 @@ describe('ProfileView Tests', () => {
     const inProgressCourses = screen.getByText(
       'ProfileView.header.coursesInprogress (1)'
     );
+    const upcomingCreatedCourses = screen.queryByText(
+      'ProfileView.header.upcomingCreatedCourses'
+    );
+    const tempaltesAdmin = screen.queryByText(
+      'ProfileView.header.templatesAdmin'
+    );
+    const tempaltesTrainer = screen.queryByText(
+      'ProfileView.header.templatesTrainer'
+    );
     expect(myCourses).toBeInTheDocument();
     expect(upcomingCourses).toBeInTheDocument();
     expect(inProgressCourses).toBeInTheDocument();
+    expect(upcomingCreatedCourses).not.toBeInTheDocument();
+    expect(tempaltesAdmin).not.toBeInTheDocument();
+    expect(tempaltesTrainer).not.toBeInTheDocument();
+  });
+  it('should render correct dropdowns for trainer', async () => {
+    (useSession as jest.Mock).mockReturnValue({
+      data: {
+        user: trainerUser,
+      },
+      status: 'authenticated',
+    });
+
+    renderWithTheme(
+      <ProfileView
+        lang="en"
+        userDetails={{
+          name: trainerUser.name,
+          email: trainerUser.email,
+          image: trainerUser.image,
+        }}
+        courses={testCourses}
+        createdCourses={
+          testCourses.filter(
+            (course) => course.createdById === trainerUser.id
+          ) || []
+        }
+        users={[]}
+        children={[]}
+        templates={[template]}
+        tags={[]}
+      />
+    );
+
+    const myCourses = screen.getByText('ProfileView.label.myCourses');
+    const upcomingCourses = screen.getByText(
+      'ProfileView.header.upcomingCourses (1)'
+    );
+    const inProgressCourses = screen.getByText(
+      'ProfileView.header.coursesInprogress (1)'
+    );
+    const upcomingCreatedCourses = screen.getByText(
+      'ProfileView.header.upcomingCreatedCourses (0)' // trainers created courses are in the past
+    );
+    const tempaltesAdmin = screen.queryByText(
+      'ProfileView.header.templatesAdmin (1)'
+    );
+    const tempaltesTrainer = screen.getByText(
+      'ProfileView.header.templatesTrainer (1)'
+    );
+    expect(myCourses).toBeInTheDocument();
+    expect(upcomingCourses).toBeInTheDocument();
+    expect(inProgressCourses).toBeInTheDocument();
+    expect(upcomingCreatedCourses).toBeInTheDocument();
+    expect(tempaltesAdmin).not.toBeInTheDocument();
+    expect(tempaltesTrainer).toBeInTheDocument();
+  });
+  it('should render correct dropdowns for admin', async () => {
+    (useSession as jest.Mock).mockReturnValue({
+      data: {
+        user: adminUser,
+      },
+      status: 'authenticated',
+    });
+
+    renderWithTheme(
+      <ProfileView
+        lang="en"
+        userDetails={{
+          name: adminUser.name,
+          email: adminUser.email,
+          image: adminUser.image,
+        }}
+        courses={testCourses}
+        createdCourses={
+          testCourses.filter((course) => course.createdById === adminUser.id) ||
+          []
+        }
+        users={[]}
+        children={[]}
+        templates={[template]}
+        tags={[]}
+      />
+    );
+
+    const myCourses = screen.getByText('ProfileView.label.myCourses');
+    const upcomingCourses = screen.getByText(
+      'ProfileView.header.upcomingCourses (1)'
+    );
+    const inProgressCourses = screen.getByText(
+      'ProfileView.header.coursesInprogress (1)'
+    );
+    const upcomingCreatedCourses = screen.getByText(
+      'ProfileView.header.upcomingCreatedCourses (1)'
+    );
+    const tempaltesAdmin = screen.getByText(
+      'ProfileView.header.templatesAdmin (1)'
+    );
+    const tempaltesTrainer = screen.queryByText(
+      'ProfileView.header.templatesTrainer (1)'
+    );
+    expect(myCourses).toBeInTheDocument();
+    expect(upcomingCourses).toBeInTheDocument();
+    expect(inProgressCourses).toBeInTheDocument();
+    expect(upcomingCreatedCourses).toBeInTheDocument();
+    expect(tempaltesAdmin).toBeInTheDocument();
+    expect(tempaltesTrainer).not.toBeInTheDocument();
   });
 });
