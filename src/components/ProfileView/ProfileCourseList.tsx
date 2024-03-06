76641e70a55157ef446d8047d9fa85ee9f71c73d
rauhja <82282638+rauhja@users.noreply.github.com>
2024-03-06 14:52:48 +0200

    Modify timer if course is in progress

--- src/components/ProfileView/ProfileCourseList.tsx
+++ src/components/ProfileView/ProfileCourseList.tsx
@@ -40,6 +40,11 @@ export default function ProfileCourseList({
     setIsCollapsed(!isCollapsed);
   };
 
+  const isCourseInProgress = (course: Course) => {
+    const currentDate = new Date();
+    return course.startDate <= currentDate && course.endDate >= currentDate;
+  };
+
   return (
     <Box
       sx={{
@@ -119,9 +124,18 @@ export default function ProfileCourseList({
                         <NoSsr>
                           <Chip
                             icon={<TimerIcon />}
-                            label={timeUntilstart(course.startDate)}
+                            label={
+                              isCourseInProgress(course)
+                                ? 'In Progress'
+                                : timeUntilstart(course.startDate)
+                            }
                             size="small"
-                            style={{ marginLeft: 'auto' }}
+                            sx={{
+                              marginLeft: 'auto',
+                            }}
+                            color={
+                              isCourseInProgress(course) ? 'success' : 'default'
+                            }
                           />
                         </NoSsr>
                       )}
