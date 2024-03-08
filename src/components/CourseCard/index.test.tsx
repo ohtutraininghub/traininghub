c17e0b4479d13b21e8593727269e4aff0ea57b71
erjav <erja.vaskivuori@helsinki.fi>
2024-03-08 10:10:13 +0200

    Fixed old test and added test for displaying past course

--- src/components/CourseCard/index.test.tsx
+++ src/components/CourseCard/index.test.tsx
@@ -20,41 +20,83 @@ jest.mock('../../lib/i18n/client', () => ({
   },
 }));
 
-const course: CourseWithInfo = {
+const currentDate = new Date().setHours(9, 0, 0, 0).valueOf();
+const msDay = 24 * 60 * 60 * 1000;
+
+const upComingCourse: CourseWithInfo = {
   id: '22',
   name: 'Test course',
   description: 'A Test course',
-  startDate: new Date(),
-  endDate: new Date(),
+  startDate: new Date(currentDate + msDay),
+  endDate: new Date(currentDate + 2 * msDay),
   lastEnrollDate: null,
   lastCancelDate: null,
+  createdBy: {
+    name: 'Test User',
+  },
   createdById: '123456789',
   maxStudents: 42,
   _count: {
     students: 0,
+    requesters: 0,
   },
   tags: [],
   image: '',
   summary: 'After this course you will know all about testing with Jest!',
 };
 
-beforeEach(() => {
-  renderWithTheme(
-    <CourseCard
-      lang="en"
-      enrolls={`0 / ${course.maxStudents}`}
-      course={course}
-    />
-  );
-});
+const pastCourse: CourseWithInfo = {
+  id: '23',
+  name: 'Second test course',
+  description: 'The second test course',
+  startDate: new Date(currentDate - 3 * msDay),
+  endDate: new Date(currentDate - msDay),
+  lastEnrollDate: null,
+  lastCancelDate: null,
+  createdBy: {
+    name: 'Test User',
+  },
+  createdById: '123456789',
+  maxStudents: 13,
+  _count: {
+    students: 0,
+    requesters: 10,
+  },
+  tags: [],
+  image: '',
+  summary: 'Test course for testing purposes!',
+};
 
 describe('CourseCard tests', () => {
-  it('Course data should be displayed correctly', async () => {
-    expect(screen.getByText(course.name)).toBeVisible();
+  it('Course data should be displayed correctly for upcoming course', async () => {
+    renderWithTheme(
+      <CourseCard
+        lang="en"
+        studentCount={`0 / ${upComingCourse.maxStudents}`}
+        course={upComingCourse}
+      />
+    );
+
+    expect(screen.getByText(upComingCourse.name)).toBeVisible();
+    expect(screen.getByText(`0 / ${upComingCourse.maxStudents}`)).toBeVisible();
     expect(
-      screen.getByText(`0 / ${course.maxStudents}`, { exact: false })
-    ).toBeVisible();
-    expect(screen.queryByText(course.description)).not.toBeInTheDocument();
-    expect(screen.getByText(course.summary as string)).toBeVisible();
+      screen.queryByText(upComingCourse.description)
+    ).not.toBeInTheDocument();
+    expect(screen.getByText(upComingCourse.summary as string)).toBeVisible();
+  });
+  it('Course data should be displayed correctly for past course', async () => {
+    renderWithTheme(
+      <CourseCard
+        lang="en"
+        studentCount={`${pastCourse._count.requesters}`}
+        course={pastCourse}
+      />
+    );
+
+    expect(screen.getByText('CourseCard.expired')).toBeVisible();
+    expect(screen.getByText(pastCourse.name)).toBeVisible();
+    expect(screen.getByText(`${pastCourse._count.requesters}`)).toBeVisible();
+    expect(screen.queryByText(pastCourse.description)).not.toBeInTheDocument();
+    expect(screen.getByText(pastCourse.summary as string)).toBeVisible();
   });
 });
