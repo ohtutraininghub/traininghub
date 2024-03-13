bfc3e66030f26ed9c380d869c9c658adf8906003
Juho <JuhoPaananen@users.noreply.github.com>
2024-03-13 14:20:49 +0200

    add test for correct sorting of requested courses

--- cypress/e2e/courseRequest.cy.ts
+++ cypress/e2e/courseRequest.cy.ts
@@ -17,4 +17,17 @@ describe('Course request', () => {
 
     cy.getCy('request-button').should('contain', 'Request');
   });
+
+  it('Course with highest request number should be sorted first', () => {
+    cy.login('trainee@test.com', 'TRAINEE');
+    cy.contains('Request trainings').click();
+    cy.contains('Git Fundamentals').click();
+    cy.getCy('request-button').click();
+    cy.contains('Request successfully sent!');
+
+    cy.getCy('course-modal-close-button').click();
+
+    // Seed includes two past courses, Docker and Git Fundamentals, with 0 requests iniatially. Courses are sorted by name and request number.
+    cy.getCy('grid-view').first().should('contain', 'Git Fundamentals');
+  });
 });
