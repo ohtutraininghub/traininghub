22f53963263e66c3e85c8c0b0f73be908adc3a06
rauhja <82282638+rauhja@users.noreply.github.com>
2024-03-21 17:56:43 +0200

    UI complete

--- src/app/[lang]/complete-profile/page.tsx
+++ src/app/[lang]/complete-profile/page.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import CompleteProfile from '@/components/CompleteProfile';
+import { getCountries } from '@/lib/prisma/country';
+import { getTitles } from '@/lib/prisma/title';
+import BackgroundContainer from '@/components/BackgroundContainer';
 
 export default async function CompleteProfilePage() {
-  return <CompleteProfile />;
+  const countries = await getCountries();
+  const titles = await getTitles();
+
+  return (
+    <BackgroundContainer>
+      <CompleteProfile countries={countries} titles={titles} />
+    </BackgroundContainer>
+  );
 }
--- src/components/CompleteProfile/index.tsx
+++ src/components/CompleteProfile/index.tsx
@@ -1,84 +1,187 @@
 'use client';
 
-import { Box, Container, MenuItem, Select, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Container,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from '@mui/material';
+import { Country, Title } from '@prisma/client';
+import { useState } from 'react';
 
-export default function CompleteProfile() {
+interface Props {
+  countries: Country[];
+  titles: Title[];
+}
+
+export default function CompleteProfile({ countries, titles }: Props) {
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedTitle, setSelectedTitle] = useState('');
+
+  const handleValueChange = (e: SelectChangeEvent<string>) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    if (name === 'countrySelect') {
+      setSelectedCountry(value);
+    } else if (name === 'titleSelect') {
+      setSelectedTitle(value);
+    }
+  };
+
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Form submitted');
+  };
+
   return (
-    <Container maxWidth="sm">
+    <Container
+      maxWidth="sm"
+      sx={{ display: 'flex', height: '100vh', alignItems: 'center' }}
+    >
       <Box
         sx={{
           p: 4,
           mb: 4,
           borderRadius: 2,
-          backgroundColor: 'coverBlue.main',
+          backgroundColor: 'surface.main',
           boxShadow: 8,
         }}
       >
-        <Typography
-          variant="h2"
-          color="secondary.contrastText"
-          textAlign="center"
-        >
+        <Typography variant="h2" color="secondary" textAlign="center" mb={4}>
           Complete your profile
         </Typography>
-        <Container>
-          <Select
-            labelId="country"
-            style={{ display: 'flex', flexWrap: 'wrap', marginBottom: 10 }}
-            color="secondary"
-            value="country"
+        <Typography
+          variant="h6"
+          color="secondary"
+          textAlign="center"
+          mb={2}
+          sx={{ color: 'secondary' }}
+        >
+          Please fill in the following information to complete your profile
+        </Typography>
+        <form
+          id="completeProfileForm"
+          data-testid="completeProfileForm"
+          onSubmit={submitForm}
+        >
+          <Container>
+            <InputLabel
+              htmlFor="countrySelect"
+              id="country-label"
+              sx={{ color: 'secondary' }}
+            >
+              Country
+            </InputLabel>
+            <Select
+              name="countrySelect"
+              id="countrySelect"
+              labelId="country-label"
+              style={{ display: 'flex', flexWrap: 'wrap', marginBottom: 10 }}
+              color="secondary"
+              value={selectedCountry}
+              onChange={handleValueChange}
+              sx={{ color: 'secondary' }}
+              MenuProps={{ disableScrollLock: true }}
+            >
+              {countries.map((country) => (
+                <MenuItem
+                  key={country.id}
+                  value={country.name}
+                  divider
+                  sx={{
+                    '&.Mui-selected': {
+                      backgroundColor: 'surface.main',
+                    },
+                    '&.Mui-selected.Mui-focusVisible': {
+                      backgroundColor: 'surface.dark',
+                    },
+                    '&:hover': {
+                      backgroundColor: 'surface.light',
+                    },
+                    '&.Mui-selected:hover': {
+                      backgroundColor: 'surface.main',
+                    },
+                  }}
+                >
+                  {country.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </Container>
+          <Container>
+            <InputLabel
+              htmlFor="titleSelect"
+              id="title-label"
+              sx={{ color: 'secondary' }}
+            >
+              Title
+            </InputLabel>
+            <Select
+              name="titleSelect"
+              id="titleSelect"
+              labelId="title-label"
+              style={{ display: 'flex', flexWrap: 'wrap', marginBottom: 10 }}
+              color="secondary"
+              value={selectedTitle}
+              onChange={handleValueChange}
+              sx={{ color: 'secondary' }}
+              MenuProps={{ disableScrollLock: true }}
+            >
+              {titles.map((title) => (
+                <MenuItem
+                  key={title.id}
+                  value={title.name}
+                  divider
+                  sx={{
+                    '&.Mui-selected': {
+                      backgroundColor: 'surface.main',
+                    },
+                    '&.Mui-selected.Mui-focusVisible': {
+                      backgroundColor: 'surface.dark',
+                    },
+                    '&:hover': {
+                      backgroundColor: 'surface.light',
+                    },
+                    '&.Mui-selected:hover': {
+                      backgroundColor: 'surface.main',
+                    },
+                  }}
+                >
+                  {title.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </Container>
+          <Container
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: '2rem',
+            }}
           >
-            <MenuItem
-              divider
+            <Button
+              type="submit"
+              color="secondary"
+              variant="contained"
               sx={{
-                '&.Mui-selected': {
-                  backgroundColor: 'surface.main',
+                color: 'white.main',
+                '&.Mui-disabled': {
+                  color: 'white.main',
                 },
-                '&.Mui-selected.Mui-focusVisible': {
-                  backgroundColor: 'surface.dark',
-                },
+                backgroundColor: 'secondary.main',
                 '&:hover': {
-                  backgroundColor: 'surface.light',
-                },
-                '&.Mui-selected:hover': {
-                  backgroundColor: 'surface.main',
+                  backgroundColor: 'secondary.light',
                 },
               }}
             >
-              Finland
-            </MenuItem>
-          </Select>
-        </Container>
-        <Container>
-          <Select
-            labelId="title"
-            style={{ display: 'flex', flexWrap: 'wrap', marginBottom: 10 }}
-            color="secondary"
-            value="country"
-          >
-            <MenuItem
-              divider
-              sx={{
-                '&.Mui-selected': {
-                  backgroundColor: 'surface.main',
-                },
-                '&.Mui-selected.Mui-focusVisible': {
-                  backgroundColor: 'surface.dark',
-                },
-                '&:hover': {
-                  backgroundColor: 'surface.light',
-                },
-                '&.Mui-selected:hover': {
-                  backgroundColor: 'surface.main',
-                },
-              }}
-            >
-              Employee
-            </MenuItem>
-            <MenuItem>Team Lead</MenuItem>
-            <MenuItem>Management</MenuItem>
-          </Select>
-        </Container>
+              Save Details
+            </Button>
+          </Container>
+        </form>
       </Box>
     </Container>
   );
