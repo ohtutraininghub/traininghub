fb46d189d34e5c297657d1e265bc8f88e196a398
akskokki <akseli.kokki@gmail.com>
2024-03-21 17:58:39 +0200

    Add participation creation to backend

---
+++ src/app/api/course/participation/route.ts
@@ -0,0 +1,48 @@
+import { getServerAuthSession } from '@/lib/auth';
+import { isTrainerOrAdmin } from '@/lib/auth-utils';
+import { translator } from '@/lib/i18n';
+import { prisma } from '@/lib/prisma';
+import { handleCommonErrors } from '@/lib/response/errorUtil';
+import {
+  errorResponse,
+  StatusCodeType,
+  successResponse,
+} from '@/lib/response/responseUtil';
+import { courseAndUserSchema } from '@/lib/zod/courses';
+import { NextRequest } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  try {
+    const { t } = await translator('api');
+    const session = await getServerAuthSession();
+    const data = await request.json();
+    const { courseId, userId } = courseAndUserSchema.parse(data);
+
+    if (!isTrainerOrAdmin(session.user)) {
+      return errorResponse({
+        message: t('Common.forbidden'),
+        statusCode: StatusCodeType.FORBIDDEN,
+      });
+    }
+
+    prisma.participation.create({
+      data: {
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+        course: {
+          connect: {
+            id: courseId,
+          },
+        },
+      },
+    });
+    return successResponse({
+      message: t('Participations.participationAdded'),
+    });
+  } catch (error) {
+    return await handleCommonErrors(error);
+  }
+}
