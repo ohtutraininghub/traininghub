1597abab6e6a28c4f974b72c710fcebb22631c88
kodtld <kodu@live.fi>
2024-03-27 14:10:04 +0200

    Remove maxLength from zod countries

--- src/lib/zod/countries.ts
+++ src/lib/zod/countries.ts
@@ -5,8 +5,6 @@ import { makeZodI18nMap } from 'zod-i18n-map';
 z.setErrorMap(zodI18nMap);
 z.setErrorMap(makeZodI18nMap({ ns: ['zod', 'custom'] }));
 
-export const maxCountryLength = 96;
-
 export const countrySchema = z.object({
   name: z
     .string()
@@ -15,14 +13,7 @@ export const countrySchema = z.object({
         i18n: { key: 'countryNameRequired' },
       },
     })
-    .refine((value) => value.trim().length <= maxCountryLength, {
-      params: {
-        i18n: {
-          key: 'maximumLengthForCountryIs',
-          values: { maxCountryLength },
-        },
-      },
-    })
+
     .refine((value) => !/\s{2,}/.test(value), {
       params: {
         i18n: { key: 'consecutiveSpacesNotAllowed' },
