5906f4f4c25858abb4edba6080ea1eec7941feb9
laurelcrelia <laurel.matt@helsinki.fi>
2024-04-03 16:40:50 +0300

    Prevent unauthorized access to the page

--- src/app/[lang]/profile/[id]/page.tsx
+++ src/app/[lang]/profile/[id]/page.tsx
@@ -1,6 +1,10 @@
 import Container from '@mui/material/Container/Container';
 import { getUserData } from '@/lib/prisma/users';
 import { Locale } from '@/lib/i18n/i18n-config';
+import { notFound } from 'next/navigation';
+import { isAdmin } from '@/lib/auth-utils';
+import UnauthorizedError from '@/components/UnauthorizedError';
+import { getServerAuthSession } from '@/lib/auth';
 
 import ProfileUserDetails from '@/components/ProfileView/ProfileUserDetails';
 
@@ -12,8 +16,16 @@ type Props = {
 };
 
 export default async function ProfilePageById({ params }: Props) {
+  const session = await getServerAuthSession();
   const userId = params.id;
   const userData = await getUserData(userId);
+
+  if (!userData) {
+    notFound();
+  }
+  if (!isAdmin(session.user)) {
+    return <UnauthorizedError lang={params.lang} />;
+  }
 
   return (
     <Container maxWidth="md">
