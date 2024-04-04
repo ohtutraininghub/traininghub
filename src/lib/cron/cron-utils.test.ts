8a4f19e628d410b1b1a6c5d4b6d54b65f581701e
aitoAarni <aarni.rechardt@gmail.com>
2024-04-04 12:55:57 +0300

    expect functions with content added to cron-utils tests
    Co-authored-by: erjavaskivuori <erja.vaskivuori@helsinki.fi>

--- src/lib/cron/cron-utils.test.ts
+++ src/lib/cron/cron-utils.test.ts
@@ -8,9 +8,12 @@ const inThreeDays = new Date();
 inThreeDays.setDate(inThreeDays.getDate() + 3);
 
 const courseInOneDay = {
-  students: {
-    email: 'email@email.com',
-  },
+  students: [
+    {
+      email: 'email1@email.com',
+    },
+    { email: 'hotmail@email.gov.uk' },
+  ],
   id: '123',
   name: 'testCourse1',
   description: 'testDescription',
@@ -27,7 +30,7 @@ const courseInOneDay = {
 
 const courseInThreeDays = {
   students: {
-    email: 'email@email.com',
+    email: 'email2@email.com',
   },
   id: '123',
   name: 'testCourse2',
@@ -58,7 +61,79 @@ jest.mock('../prisma/courses', () => ({
 describe('Tests for cron-utils', () => {
   it('should call sendReminderToUsers with the correct parameters', () => {
     sendNotificationsBeforeCourseStart();
-    expect(mockSendReminderToUsers).toHaveBeenCalledTimes(2);
-    expect(mockCoursesWithStartDateBetweenDates).toHaveBeenCalledWith();
+    expect(mockSendReminderToUsers).toHaveBeenNthCalledWith(
+      1,
+      {
+        students: [
+          {
+            email: 'email1@email.com',
+          },
+          { email: 'hotmail@email.gov.uk' },
+        ],
+        id: '123',
+        name: 'testCourse1',
+        description: 'testDescription',
+        summary: 'testSummary',
+        startDate: inOneDay,
+        endDate: inOneDay,
+        lastEnrollDate: null,
+        lastCancelDate: null,
+        maxStudents: '10',
+        createdById: '123',
+        image: null,
+        slackChannelId: '123',
+      },
+      'email1@email.com',
+      '1 day'
+    );
+
+    expect(mockSendReminderToUsers).toHaveBeenNthCalledWith(
+      2,
+      {
+        students: [
+          {
+            email: 'email1@email.com',
+          },
+          { email: 'hotmail@email.gov.uk' },
+        ],
+        id: '123',
+        name: 'testCourse1',
+        description: 'testDescription',
+        summary: 'testSummary',
+        startDate: inOneDay,
+        endDate: inOneDay,
+        lastEnrollDate: null,
+        lastCancelDate: null,
+        maxStudents: '10',
+        createdById: '123',
+        image: null,
+        slackChannelId: '123',
+      },
+      'hotmail@email.gov.uk',
+      '1 day'
+    );
+
+    expect(mockSendReminderToUsers).toHaveBeenNthCalledWith(
+      3,
+      {
+        students: {
+          email: 'email2@email.com',
+        },
+        id: '123',
+        name: 'testCourse2',
+        description: 'testDescription',
+        summary: 'testSummary',
+        startDate: inThreeDays,
+        endDate: inThreeDays,
+        lastEnrollDate: null,
+        lastCancelDate: null,
+        maxStudents: '10',
+        createdById: '123',
+        image: null,
+        slackChannelId: '123',
+      },
+      'email2@email.com',
+      '3 days'
+    );
   });
 });
