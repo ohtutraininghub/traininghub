afadb7802150c9b6223b35efc7979dbd4a06f7aa
kodtld <kodu@live.fi>
2024-04-10 13:34:55 +0300

    Boilerplate for papaparse

---
+++ src/lib/papaparse/index.ts
@@ -0,0 +1,10 @@
+// import Papa from 'papaparse';
+
+// const csv = Papa.unparse(data);
+// const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+// const link = document.createElement('a');
+// link.href = URL.createObjectURL(blob);
+// link.setAttribute('download', 'filename.csv');
+// document.body.appendChild(link);
+// link.click();
+// document.body.removeChild(link);
